84d2c8273ea111e6ba5bb2e929c656441e3d0034
RotachNastya <education.for.nastya@gmail.com>
2024-12-19 14:58:21 +0500

    task0

--- Tyuiu.RotachAO.Sprint6.Task0.V30.Lib/DataService.cs
+++ Tyuiu.RotachAO.Sprint6.Task0.V30.Lib/DataService.cs
@@ -4,6 +4,14 @@ namespace Tyuiu.RotachAO.Sprint6.Task0.V30.Lib
 {
     public class DataService : ISprint6Task0V30
     {
+        public double Calculate(int x)
+        {
+            if (x < -3)
+            {
+                throw new ArgumentOutOfRangeException(nameof(x), "Аргумент x должен быть >= -3, чтобы избежать комплексных значений.");
+            }
 
+            return Math.Round(x * Math.Sqrt(x + 3),3);
+        }
     }
 }
