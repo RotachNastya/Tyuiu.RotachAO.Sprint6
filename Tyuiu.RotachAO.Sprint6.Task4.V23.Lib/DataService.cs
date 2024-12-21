9d5c6cfc7f70ee26690c7b01e1898c8a2c742abc
RotachNastya <education.for.nastya@gmail.com>
2024-12-21 15:13:01 +0500

    task4

--- Tyuiu.RotachAO.Sprint6.Task4.V23.Lib/DataService.cs
+++ Tyuiu.RotachAO.Sprint6.Task4.V23.Lib/DataService.cs
@@ -4,6 +4,20 @@ namespace Tyuiu.RotachAO.Sprint6.Task4.V23.Lib
 {
     public class DataService : ISprint6Task4V23
     {
-
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                y = Math.Round(4 - 2 * i + (2 + Math.Cos(i))/(2 * i - 2), 2);
+                valueArray[count] = y;
+                count++;
+            }
+            return valueArray;
+        }
     }
 }
