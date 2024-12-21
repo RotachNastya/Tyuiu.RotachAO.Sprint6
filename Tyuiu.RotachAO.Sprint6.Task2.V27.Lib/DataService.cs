a24f8314f15c0ed7e828ad06a1e120af73eb156b
RotachNastya <education.for.nastya@gmail.com>
2024-12-21 15:01:47 +0500

    task2

--- Tyuiu.RotachAO.Sprint6.Task2.V27.Lib/DataService.cs
+++ Tyuiu.RotachAO.Sprint6.Task2.V27.Lib/DataService.cs
@@ -4,30 +4,20 @@ namespace Tyuiu.RotachAO.Sprint6.Task2.V27.Lib
 {
     public class DataService : ISprint6Task2V27
     {
-        public static double Calculate(double x)
-        {
-            double denominator = Math.Cos(x) -2*x;
-            if (Math.Abs(denominator) < 1e-9) // Проверка на деление на ноль
-            {
-                return 0;
-            }
-
-            double result = (2*x -3) / denominator + 5 * x - Math.Sin(x);
-            return Math.Round(result, 2); // Округление до 2 знаков
-        }
-
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            int size = stopValue - startValue + 1;
-            double[] results = new double[size];
-
-            for (int i = 0; i < size; i++)
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int i = startValue; i <= stopValue; i++)
             {
-                double x = startValue + i;
-                results[i] = Calculate(x);
+                y = Math.Round((2 * i - 3) / (Math.Cos(i) - 2* i) + 5 - Math.Sin(i), 2);
+                valueArray[count] = y;
+                count++;
             }
-
-            return results;
+            return valueArray;
         }
     }
 }
